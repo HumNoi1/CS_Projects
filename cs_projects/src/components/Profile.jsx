393ee69868dbdb6dc548c23399335a62f270450b
Thanawat Kanphuton <framegg908@gmail.com>
2024-11-11 10:36:14 +0700

    "first commit"

---
+++ cs_projects/src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const user = {
+    name: 'John Doe',
+}
+
+function Profile() {
+  return (
+    <div>
+      <h1>Cs project</h1>
+      <hr />
+    </div>
+  )
+}
+
+export default Profile
