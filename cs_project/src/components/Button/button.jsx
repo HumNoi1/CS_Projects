049b8727cda55e3081093f6f99c53e26f9629ea5
Thanawat Kanphuton <framegg908@gmail.com>
2024-11-11 14:39:32 +0700

    "update"

--- cs_project/src/components/Button/button.jsx
+++ cs_project/src/components/Button/button.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Button = () => {
   return (
-    <button className="px-8 py-3 text-lg font-semibold rounded-full 
-                     bg-gradient-to-r from-purple-500 to-cyan-500
-                     hover:from-purple-600 hover:to-cyan-600
-                     text-white transition-all duration-300
-                     transform hover:scale-105">
-      Get started
-    </button>
+    <button
+  type="button"
+  data-ripple-light="true"
+  class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+>
+  Button
+</button>
   );
 };
 
