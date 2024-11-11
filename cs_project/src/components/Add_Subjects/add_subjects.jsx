51239cb816c36e47ed1fcdee086b68effc4664ed
Thanawat Kanphuton <framegg908@gmail.com>
2024-11-11 15:14:29 +0700

    "update code"

--- cs_project/src/components/Add_Subjects/add_subjects.jsx
+++ cs_project/src/components/Add_Subjects/add_subjects.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from 'react'
 
-const Button = () => {
+function add_subjects() {
   return (
-    <button
-  type="button"
+    <div>
+      <h1>Term</h1>
+      <h1>Class</h1>
+      <h1>Subjects</h1>
+
+      <button type="button"
   data-ripple-light="true"
   class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
 >
-  Get started
-</button>
-  );
-};
+        confirm
+      </button>
+    </div>
+  )
+}
 
-export default Button;
+export default add_subjects
---
+++ cs_project/src/components/Home_page/home_page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function home_page() {
+  return (
+    <div>
+      <h1>Hello User</h1>
+      <button>Add</button>
+    </div>
+  )
+}
+
+export default home_page
